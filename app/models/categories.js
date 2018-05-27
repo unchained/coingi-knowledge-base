4593d04cfba8c46ef373b48d75d2d48e9c7e6e7f
ParallelUniv3rse <arnost@unchained.studio>
2018-05-27 15:40:21 +0200

    Dynamic links and content

---
+++ app/models/categories.js
@@ -0,0 +1,64 @@
+const demoArticlesList = [
+  'what-is-money',
+  'i-have-bought/sold-a-currency-but-don\'t-see-the-right-amount-on-my-account\'s-balance',
+  'why-am-i-not-rich-yet',
+  'my-dog-ate-my-hardware-wallet',
+  'billion-dollar-pizza',
+  'history-of-trading',
+  'banking-on-bitcoin',
+  'leverage-moving-average',
+  'who-is-the-real-satoshi',
+];
+const demoSectionsList = {
+  "Let's talk money": {
+    icon: 'fa-money-bill-alt',
+    articles: demoArticlesList,
+  },
+  'Trading strategies': {
+    icon: 'fa-chart-line',
+    articles: demoArticlesList,
+  },
+  'Security is key': {
+    icon: 'fa-lock-alt',
+    articles: demoArticlesList,
+  },
+};
+
+module.exports = {
+  'my-account': {
+    name: 'My account',
+    description: 'Editing your profile, Two factor authentication, verifying your account',
+    icon: 'fa-user',
+    sections: demoSectionsList,
+  },
+  'coin-transfer': {
+    name: 'Coin transfer',
+    description: 'Sending and recieving Bitcoin, Transfer fees',
+    icon: 'fa-exchange-alt',
+    sections: demoSectionsList,
+  },
+  'deposits-and-withdrawals': {
+    name: 'Deposits and withdrawals',
+    description: 'Sending and withdrawing money from/to your Coingi account',
+    icon: 'fa-money-bill-alt',
+    sections: demoSectionsList,
+  },
+  trading: {
+    name: 'Trading',
+    description: 'Buying and selling Bitcoin, Market analysis',
+    icon: 'fa-chart-area',
+    sections: demoSectionsList,
+  },
+  notifications: {
+    name: 'Notifications',
+    description: 'Market signals, Managing notifications from Coingi',
+    icon: 'fa-bell',
+    sections: demoSectionsList,
+  },
+  general: {
+    name: 'General',
+    description: 'What is Bitcoin?, How does it work?',
+    icon: 'fa-question-circle',
+    sections: demoSectionsList,
+  },
+};
